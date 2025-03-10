b0a729d077d6f57c867b89fc2a8f8a0fbbc006ea
01111000x <jancel.ensina@gmail.com>
2025-03-10 22:25:53 +0800

    added redirect urls in settings.py

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -121,4 +121,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = '/recipes/list/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
