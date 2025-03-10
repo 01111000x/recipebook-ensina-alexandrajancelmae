0f0e5978fd3f379c0133bc2ff9a4f91e72307b4b
01111000x <jancel.ensina@gmail.com>
2025-03-10 22:18:43 +0800

    restricted Recipe Detail to logged-in users

--- ledger/views.py
+++ ledger/views.py
@@ -1,17 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Recipe
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
-    context = {
-        "recipes": recipes,
-    }
-    return render(request, 'ledger/recipe_list.html', context)
+    return render(request, 'ledger/recipe_list.html', {"recipes": recipes})
 
+@login_required
 def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
-    context = {
-        "recipe": recipe,
-    }
-    return render(request, 'ledger/recipe_detail.html', context)
+    return render(request, 'ledger/recipe_detail.html', {"recipe": recipe})
 
