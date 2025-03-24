3b9dc5a64f2f731ad371d0c483d1fa88ef44c61f
01111000x <jancel.ensina@gmail.com>
2025-03-24 21:33:42 +0800

    added RecipeImage in admin

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient
+from .models import Recipe, Ingredient, RecipeIngredient, RecipeImage
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
     extra = 1
     
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+    extra = 1
+    
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [RecipeIngredientInline]
+    inlines = [RecipeImageInline]
     
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient)
