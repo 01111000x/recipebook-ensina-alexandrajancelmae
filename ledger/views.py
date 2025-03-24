db81bdad0d0ad76c7f6084d3f92111431c18d64c
01111000x <jancel.ensina@gmail.com>
2025-03-24 21:38:24 +0800

    added RecipeCreateView in views.py

--- ledger/views.py
+++ ledger/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
 from .models import Recipe
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    fields = ['name']
+    template_name = 'ledger/recipe_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('recipe_detail', kwargs={'pk': self.object.pk})
 
 def recipe_list(request):
     recipes = Recipe.objects.all()
